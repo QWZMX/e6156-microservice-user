ff99d19e9c4471781ebce440f0e4f346d24645d7
Yufeng Wan <yw3921@columbia.edu>
2023-10-21 16:26:11 -0400

    set up flask cors

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI, Response
 
 # I like to launch directly and not use the standard FastAPI startup
 import uvicorn
+from flask import Flask,jsonify
+from flask_cors import CORS
 
 
-app = FastAPI()
+app = Flask(__name__)
+CORS(app)
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.route("/", methods=['GET'])
+def root():
+    return jsonify(message="Hello World")
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8012)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host="0.0.0.0", port=8012)
